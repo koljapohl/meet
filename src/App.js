16b5c9009754d3193bd00cf295308724d43b1399
Kolja Pohl <kolja.pohl@pohlcon.com>
2021-05-13 07:55:32 +0200

    Scenario 1 fully tested

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import EventList from './EventList';
+import CitySearch from './CitySearch';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <CitySearch />
         <EventList />
       </div>
     );
