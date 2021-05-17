3c2d3393939d5dfa7f0d02f178b507c7488d3adb
Kolja Pohl <kolja.pohl@pohlcon.com>
2021-05-17 10:54:39 +0200

    added + tested feature 2

--- src/Event.js
+++ src/Event.js
@@ -1,22 +1,57 @@
 import React, { Component } from 'react';
 
 class Event extends Component {
+  state = {
+    showDetails: false
+  };
+
+  handleClick = () => {
+    this.setState( previousState => ( {
+      showDetails: !previousState.showDetails
+    } ) );
+  }
+
   render() {
     const { event } = this.props;
+    const { showDetails } = this.state;
+
+    const eventISODateTime = new Date( event.start.dateTime );
+    const eventDate = eventISODateTime.toDateString();
+    const eventTime = eventISODateTime.toTimeString();
+    const eventTimeFormatted = `${eventTime.slice( 0, 5 )} ${eventTime.slice( 18 )}`;
+
     return (
       <div className="event">
         <div className="event-summary">
-          <h2 className="event-header">
-            {event.summary}
-          </h2>
-          <div className="event-date">
-            {event.start.dateTime}
+          <h2 className="event-title">{event.summary}</h2>
+          <div className="event-location">{event.location}</div>
+          <div className="event-time-date">
+            <p className="event-date">{eventDate}</p>
+            <p className="event-time">{eventTimeFormatted}</p>
           </div>
-          <div claassName="event-title">@{event.summary}</div>
-          <div className="event-location"> | {event.location}</div>
         </div>
+
+        {showDetails ?
+          <div>
+            <div className="event-details">
+              <a href={event.htmlLink}>See details on Google Calendar</a>
+              <div className="event-description">{event.description}</div>
+            </div>
+            <button
+              className="hide-details-btn"
+              onClick={this.handleClick}>
+              Hide details
+            </button>
+          </div>
+          :
+          <button
+            className="show-details-btn"
+            onClick={this.handleClick}>
+            show details
+          </button>
+        }
       </div>
-    )
-  }
+    );
+  };
 }
 export default Event;
--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -9,6 +9,11 @@ describe( '<Event /> component', () => {
   beforeAll( () => {
     EventWrapper = shallow( <Event event={event} /> );
   } );
+  beforeEach( () => {
+    EventWrapper.setState( {
+      showDetails: false
+    } );
+  } );
 
   test( 'render event element', () => {
     expect( EventWrapper.find( '.event' ) ).toHaveLength( 1 );
@@ -19,6 +24,46 @@ describe( '<Event /> component', () => {
   } );
 
   test( 'render event summary content', () => {
-    expect( EventWrapper.find( '.event-summary' ).children() ).toHaveLength( 4 );
+    expect( EventWrapper.find( '.event-summary' ).children() ).toHaveLength( 3 );
+  } );
+
+  test( 'render details button', () => {
+    expect( EventWrapper.find( 'button' ) ).toHaveLength( 1 );
+  } );
+
+  test( 'change state when clicking button', () => {
+    EventWrapper.find( 'button' ).simulate( 'click' );
+    expect( EventWrapper.state( 'showDetails' ) ).toBe( true );
+  } );
+
+  test( 'show event details when button is clicked', () => {
+    EventWrapper.find( 'button' ).simulate( 'click' );
+    expect( EventWrapper.state( 'showDetails' ) ).toBe( true );
+    expect( EventWrapper.find( '.event-details' ) ).toHaveLength( 1 );
+    expect( EventWrapper.find( '.event-details' ).children() ).toHaveLength( 2 );
+  } );
+
+  test( 'toggle rendering of event details with each click', () => {
+    EventWrapper.find( 'button' ).simulate( 'click' );
+    expect( EventWrapper.state( 'showDetails' ) ).toBe( true );
+    expect( EventWrapper.find( '.event-details' ) ).toHaveLength( 1 );
+    expect( EventWrapper.find( '.event-details' ).children() ).toHaveLength( 2 );
+
+    EventWrapper.find( 'button' ).simulate( 'click' );
+    expect( EventWrapper.state( 'showDetails' ) ).toBe( false );
+    expect( EventWrapper.find( '.event-details' ) ).toHaveLength( 0 );
+    expect( EventWrapper.find( '.event-details' ).children() ).toHaveLength( 0 );
+  } );
+
+  test( 'toggle show/hide wording within button', () => {
+    EventWrapper.find( 'button' ).simulate( 'click' );
+    expect( EventWrapper.state( 'showDetails' ) ).toBe( true );
+    expect( EventWrapper.find( '.hide-details-btn' ) ).toHaveLength( 1 );
+    expect( EventWrapper.find( '.show-details-btn' ) ).toHaveLength( 0 );
+
+    EventWrapper.find( 'button' ).simulate( 'click' );
+    expect( EventWrapper.state( 'showDetails' ) ).toBe( false );
+    expect( EventWrapper.find( '.hide-details-btn' ) ).toHaveLength( 0 );
+    expect( EventWrapper.find( '.show-details-btn' ) ).toHaveLength( 1 );
   } );
 } )
