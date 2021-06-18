5054a61939355b615037c146628625326f646784
Kolja Pohl <kolja.pohl@pohlcon.com>
2021-06-18 08:20:36 +0200

    [ADD] Tests Feature 2

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -13,8 +13,8 @@ const feature = loadFeature('./src/features/filterEventsByCity.feature');
 defineFeature(feature, test => {
   test('When user hasn’t searched for a city, show upcoming events from all cities.', ({ given, when, then }) => {
     given('user hasn\'t searched for any city', () => {
-
     });
+
     let AppWrapper;
     when('user opens the app', () => {
       AppWrapper = mount(<App />);
