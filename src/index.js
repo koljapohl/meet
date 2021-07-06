1181d3d9c023e1f8d489e5e7354a29c3e5dfbffe
Kolja Pohl <kolja.pohl@pohlcon.com>
2021-07-06 22:49:56 +0200

    Revert "[ADD] Atatus APM tool integrated"
    This reverts commit 0290614fd0e8dd589ffc34c3d77159aabdd22e1e.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import * as atatus from 'atatus-spa';
-
-atatus.config('8387138947a3442484c08052a9f18d97').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -24,5 +21,3 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-
-atatus.notify(new Error('Test Atatus Setup'));
