4d8cd7feca3f2db6c3001f2f6d8c13d9fde57b2b
Kolja Pohl <kolja.pohl@pohlcon.com>
2021-08-10 14:33:28 +0200

    [FIX] data format in localStorage

--- src/api.js
+++ src/api.js
@@ -42,6 +42,9 @@ export const getEvents = async () => {
 
   if (!navigator.onLine) {
     const data = localStorage.getItem('lastEvents');
+    console.log(data);
+    console.log('data: ' + JSON.parse(data));
+    console.log(JSON.parse('data.events: ' + data.events));
     NProgress.done();
     return data ? JSON.parse(data).events : [];
   }
@@ -53,7 +56,7 @@ export const getEvents = async () => {
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
-      localStorage.setItem('lastEvents', JSON.stringify(result.data));
+      localStorage.setItem('lastEvents', JSON.stringify(result.data.events));
       localStorage.setItem('locations', JSON.stringify(locations));
     }
     NProgress.done();
