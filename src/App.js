1915aec7757f59a1434fe7806eb0723b6b85dcf1
Kolja Pohl <kolja.pohl@pohlcon.com>
2021-08-12 10:10:07 +0200

    [ADD] chart visualizing number of events per city

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import WelcomeScreen from './WelcomeScreen';
 import './App.css';
 import { extractLocations, getEvents, checkToken, getAccessToken } from './api';
 import { InfoAlert } from './Alert';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 class App extends Component {
   state = {
@@ -19,6 +20,18 @@ class App extends Component {
 
   async componentDidMount () {
     this.mounted = true;
+    if (window.location.href.startsWith('http://localhost')) {
+      this.setState({ showWelcomeScreen: false });
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({
+            events,
+            locations: extractLocations(events)
+          });
+        }
+      });
+      return;
+    }
     const accessToken = localStorage.getItem('access_token');
     const isTokenValid = (await checkToken(accessToken)).error ? false : true;
     const searchParams = new URLSearchParams(window.location.search);
@@ -60,6 +73,16 @@ class App extends Component {
     });
   };
 
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length;
+      const city = location.split(', ').shift();
+      return { city, number };
+    });
+    return data;
+  };
+
   render () {
     if (this.state.showWelcomeScreen === undefined) return <div className="App" />;
 
@@ -71,6 +94,25 @@ class App extends Component {
         }
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents amountEvents={this.state.amountEvents} updateEvents={this.updateEvents} />
+        <h4>Events in each city</h4>
+
+        <ResponsiveContainer height={400}>
+          <ScatterChart
+            margin={{
+              top: 20,
+              right: 20,
+              bottom: 20,
+              left: 20,
+            }}
+          >
+            <CartesianGrid strokeDasharray="5 5" />
+            <XAxis type="category" dataKey="city" name="City" stroke="#ccc" />
+            <YAxis type="number" dataKey="number" name="Number of events" stroke="#ccc" />
+            <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+            <Scatter data={this.getData()} fill="#17a2b8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+
         <EventList events={this.state.events} />
         <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen} getAccessToken={() => { getAccessToken(); }} />
       </div>
