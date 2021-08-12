5d6eb38a91c6db4cf6100b9375b882d9152709c3
Kolja Pohl <kolja.pohl@pohlcon.com>
2021-08-12 11:42:29 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/meet/precache-manifest.9a5a1d1cb0a23b6b7d7842c149f067c1.js"
+  "/meet/precache-manifest.06d470731f8a5477c80f1e7c06d9a4c6.js"
 );
 
 workbox.clientsClaim();
