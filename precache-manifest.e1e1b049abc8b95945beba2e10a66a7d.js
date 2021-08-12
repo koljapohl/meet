a5ec3158ba65bff1cbdd445c0d62507a87eeef9a
Kolja Pohl <kolja.pohl@pohlcon.com>
2021-08-12 13:51:26 +0200

    Updates

---
+++ precache-manifest.e1e1b049abc8b95945beba2e10a66a7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bb220bb034d5e6c776b",
+    "url": "/meet/static/css/main.6d7c8373.chunk.css"
+  },
+  {
+    "revision": "6bb220bb034d5e6c776b",
+    "url": "/meet/static/js/main.deeff5cc.chunk.js"
+  },
+  {
+    "revision": "3c9b0feaa01fa07c704a",
+    "url": "/meet/static/js/runtime~main.642f153b.js"
+  },
+  {
+    "revision": "51ac4b3841caa6880e25",
+    "url": "/meet/static/js/2.02f7f2ef.chunk.js"
+  },
+  {
+    "revision": "aa570962f7a78d864d3b",
+    "url": "/meet/static/js/3.b87d422b.chunk.js"
+  },
+  {
+    "revision": "7a6fed0c929212b90db936a6abb1b561",
+    "url": "/meet/index.html"
+  }
+];
